3144666267bf2027f35ca5732c53629a851291b3
nabin <nkbhaie2017@gmail.com>
2022-11-13 04:43:12 -0700

    refacot and clean up the code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
+
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { decode } from 'jsonwebtoken';
+import Routes from './Routes';
+import Navigation from './Navigation';
+// import JoblyApi from './JoblyApi';
+import UserContext from './UserContext';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [user, setUser] = useState(null);
+	const [infoLoaded, setInfoLoaded] = useState(false);
+
+	const initialValue = localStorage.getItem('jobly-token') || null;
+	const [token, setToken] = useState(initialValue);
+
+	useEffect(() => {
+		async function getUser() {
+			try {
+				let { username } = decode(token);
+				let currentUser = await JoblyApi.getUser(username);
+				setUser(currentUser);
+			} catch (err) {
+				setUser(null);
+			}
+			setInfoLoaded(true);
+		}
+
+		if (!token) {
+			localStorage.removeItem('jobly-token');
+		} else {
+			localStorage.setItem('jobly-token', token);
+		}
+		setInfoLoaded(false);
+		getUser();
+	}, [token]);
+
+	const logout = () => {
+		setUser(null);
+		setToken(null);
+	};
+
+	if (!infoLoaded) {
+		return (
+			<div style={{ display: 'flex', justifyContent: 'center', marginTop: '4em' }}>
+				<h5>Loading...</h5>
+			</div>
+		);
+	}
+
+	return (
+		<div className='App'>
+			<BrowserRouter>
+				<UserContext.Provider value={{ user, setUser }}>
+					<Navigation logout={logout} />
+					<Routes setToken={setToken} />
+				</UserContext.Provider>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
+
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
